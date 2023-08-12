297c9696b8c8a57bbf52f64cb13a0566013a5e7c
Kiran <krnsundal@gmail.com>
2023-08-12 10:58:25 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//  printing things to the console
+//        sout is shortcut for multiline print
+        System.out.println("Hello");
+        System.out.println("Hello again");
+
+        int myNum = 12;
+        int myNum2 = 5;
+        int res = myNum + myNum2;
+        System.out.println("my addition result" +  " " + res);
+
+
+//        Data Types
+        /*
+        *  int, bool string float(6-7 decimals), long, double(15 decimal places)
+        * Charactor has to be in single quotes, strings have to be double quotes
+        * */
+
+        int myInt = 44;
+        String myString = "Kiran";
+        float myFloat = 2.2f;
+        double myDouble = 3.1415926535;
+        long myLong = 100000000000000000L;
+        char myChar = 'p';
+
+
+    }
+}
